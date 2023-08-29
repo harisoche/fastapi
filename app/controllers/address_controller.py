c668db7d1cdc3db04d9cf3b6d9838686c4e3ec60
oche <haris.gumilar@sekolahmu.co.id>
2023-08-29 17:01:40 +0700

    rename method name

--- app/controllers/address_controller.py
+++ app/controllers/address_controller.py
@@ -9,17 +9,17 @@ router = APIRouter(
     )
 
 @router.post("/", response_model=AddressResponseSchema, status_code=status.HTTP_201_CREATED)
-async def create_user(address: AddressBaseSchema):
+async def create_address(address: AddressBaseSchema):
     return await AddressLogic.create(address)
 
 @router.get("/{id}", response_model=AddressResponseSchema, status_code=status.HTTP_200_OK)
-async def get_user_by_id(id: int):
+async def get_address_by_id(id: int):
     return await AddressLogic.get_by_id(id)
 
 @router.put("/", response_model=AddressResponseSchema, status_code=status.HTTP_200_OK)
-async def update_user(address: AddressUpdateSchema):
+async def update_address(address: AddressUpdateSchema):
     return await AddressLogic.update(address)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(id: int):
+async def delete_address(id: int):
     return await AddressLogic.delete(id)
